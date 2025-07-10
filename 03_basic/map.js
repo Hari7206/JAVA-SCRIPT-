43952c0a02b90116c806cdfa7b618701907af79a
Hari7206 <harithapa4654@gmail.com>
2025-07-10 16:38:30 +0530

    chainig

--- 03_basic/map.js
+++ 03_basic/map.js
@@ -9,3 +9,7 @@ const newnums = mynum.map((num) => num + 10 )
 console.log(newnums);
 console.log(ymnum);
 
+// so from here we will learn that about the chaining
+// so this is called the chaining  where we add the multiple 1s 
+const chain = mynum.map((num) => num * 10).map((num) => num +1).filter((num) => num > 31)
+console.log(chain);
