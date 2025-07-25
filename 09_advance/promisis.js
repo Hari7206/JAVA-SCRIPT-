c6f3925d373775a1b839f6969d8b8b8a082ca132
Hari Thapa <harithapa4654@gmail.com>
2025-07-25 22:47:57 +0530

    new thing

---
+++ 09_advance/promisis.js
@@ -0,0 +1,7 @@
+//  so promise itself is ana object so we get this promise in the es6 
+//  so we also have q and bluebird at old time when we dont have the promise so at that we had the q and the bluebird things 
+const promiseOne =  new Promise((resolve , reject) => {
+// so we have the call back in the promise and we have resolve and the reject in the promise 
+
+// so we have the .then function which run with the reolve conection 
+});
