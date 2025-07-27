c8128bfde44dd6edd9a22abe4211b313fd2bcafd
Hari Thapa <harithapa4654@gmail.com>
2025-07-27 12:16:03 +0530

    ok

--- 09_advance/promisis.js
+++ 09_advance/promisis.js
@@ -1,7 +1,12 @@
 //  so promise itself is ana object so we get this promise in the es6 
 //  so we also have q and bluebird at old time when we dont have the promise so at that we had the q and the bluebird things 
-const promiseOne =  new Promise((resolve , reject) => {
+// const promiseOne =  new Promise((resolve , reject) => {
 // so we have the call back in the promise and we have resolve and the reject in the promise 
 
-// so we have the .then function which run with the reolve conection 
-});
+// so we have the .then function which run with the reolve conection  and we use the .catchs for the error and then we will use the finally keyword for the  confirm that is that things is done or not so we will try the async and await with the help of try and catch value 
+// });
+
+
+
+
+
