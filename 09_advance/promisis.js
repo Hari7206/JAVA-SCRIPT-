3b08b868bc10b9f35b9f7269d02440c5fe615118
Hari Thapa <harithapa4654@gmail.com>
2025-07-28 22:16:04 +0530

    lexical scoping

--- 09_advance/promisis.js
+++ 09_advance/promisis.js
@@ -1,10 +1,16 @@
-//  so promise itself is ana object so we get this promise in the es6 
-//  so we also have q and bluebird at old time when we dont have the promise so at that we had the q and the bluebird things 
-// const promiseOne =  new Promise((resolve , reject) => {
-// so we have the call back in the promise and we have resolve and the reject in the promise 
+/*
+ so promise itself is ana object so we get this promise in the es6 
+ so we also have q and bluebird at old time when we dont have the promise so at that we had the q and the bluebird things 
+const promiseOne =  new Promise((resolve , reject) => {
+so we have the call back in the promise and we have resolve and the reject in the promise 
 
-// so we have the .then function which run with the reolve conection  and we use the .catchss for the error and then we will use the finally keyword for the  confirm that is that things is done or not so we will try the async and await with the help of try and catch value 
-// });
+so we have the .then function which run with the reolve conection  and we use the .catchss for the error and then we will use the finally keyword for the  confirm that is that things is done or not so we will try the async and await with the help of try and catch valu
+
+so in the lexical function we got to know that innter funciton can avainlable for the innter function like we have nested function so the innter function can get the data of the outer function so this is closure also 
+
+
+});
+*/ 
 
 
 
